fa79a53258cc6caba33fc0da404ecde0dbb8a740
lndrmts <mateus6leandro@gmail.com>
2020-05-11 11:15:17 -0300

    Challenge completed successfully

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,20 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
+  public async execute(id: string): Promise<void> {
     // TODO
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const transction = await transactionsRepository.findOne(id);
+
+    if (!transction) {
+      throw new AppError('Transaction does not exist');
+    }
+
+    await transactionsRepository.remove(transction);
   }
 }
 
